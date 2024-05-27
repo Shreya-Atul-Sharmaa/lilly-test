92e2e4344035f8736e1e7b3b6d3c057e4668962c
Shreya-Atul-Sharmaa <shreyaatulsharma@gmail.com>
2024-05-27 08:58:21 +0000

    Update generate_and_store_vectors.py

--- generate_and_store_vectors.py
+++ generate_and_store_vectors.py
@@ -1,38 +1,43 @@
+import os
 import random
-from elasticsearch import Elasticsearch
-
-
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 
 # Define your OpenSearch endpoint
-OPENSEARCH_ENDPOINT = "YOUR_OPENSEARCH_ENDPOINT"
+OPENSEARCH_ENDPOINT = "https://search-opensearch-for-vectors-poc-5fqfu5hcbglmswud5kcngko4ae.us-east-1.es.amazonaws.com"
 # Define your OpenSearch index
 INDEX_NAME = "vectors"
 
+# AWS
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+REGION = os.getenv('AWS_REGION')
 
+awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, REGION, 'es')
 
 # Connect to OpenSearch
-es = Elasticsearch([OPENSEARCH_ENDPOINT])
-
-
+es = Elasticsearch(
+    hosts=[{'host': OPENSEARCH_ENDPOINT, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
 
 # Function to generate random vectors
 def generate_random_vector(dim):
-    return [random.random() for _ in range(dim)]
-
-
+    return [random.random() for _ in range(dim)]
 
 # Function to store vectors in OpenSearch
 def store_vectors(num_vectors, dim):
-    for i in range(num_vectors):
-        vector = generate_random_vector(dim)
-        doc = {"vector": vector}
-        es.index(index=INDEX_NAME, body=doc)
-
-
+    for i in range(num_vectors):
+        vector = generate_random_vector(dim)
+        doc = {"vector": vector}
+        es.index(index=INDEX_NAME, body=doc)
 
 # Example usage
 if __name__ == "__main__":
-    num_vectors = 10  # Number of vectors to generate
-    dim = 10  # Dimensionality of each vector
-    store_vectors(num_vectors, dim)
-    print(f"{num_vectors} random vectors stored in OpenSearch.")
+    num_vectors = 10  # Number of vectors to generate
+    dim = 10  # Dimensionality of each vector
+    store_vectors(num_vectors, dim)
+    print(f"{num_vectors} random vectors stored in OpenSearch.")
